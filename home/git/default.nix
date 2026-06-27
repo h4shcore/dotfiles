b336e96438e0c784f4e0fa50ac07fb2d74008c30
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-28 03:15:40 +0530

    bleh :P

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,5 +1,17 @@
 {...}: {
   programs.git = {
     enable = true;
+
+    settings = {
+      user = {
+        name= "h4shcore";
+        email= "97403914+h4shcore@users.noreply.github.com";
+      };
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+      };
+    };
   };
 }
