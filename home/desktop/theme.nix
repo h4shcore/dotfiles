5cc080d360c868b733960b5a5fdb1b140dffe4cd
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-28 17:36:44 +0530

    added a cursor theme

--- home/desktop/theme.nix
+++ home/desktop/theme.nix
@@ -7,6 +7,17 @@
       package = pkgs.gruvbox-gtk-theme;
     };
 
+    iconTheme = {
+      name = "Gruvbox-Plus-Dark";
+      package = pkgs.gruvbox-dark-icons-gtk;
+    };
+
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Original-Classic";
+      size = 20;
+    };
+
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
