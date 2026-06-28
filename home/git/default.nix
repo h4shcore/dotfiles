43d40004d11b455b4324e64d3191dc0b23faf3a2
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-28 17:45:49 +0530

    added jujutsu

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,16 +1,41 @@
 {...}: {
-  programs.git = {
-    enable = true;
+  programs = {
+    git = {
+      enable = true;
 
-    settings = {
-      user = {
-        name = "h4shcore";
-        email = "97403914+h4shcore@users.noreply.github.com";
+      settings = {
+        user = {
+          name = "h4shcore";
+          email = "97403914+h4shcore@users.noreply.github.com";
+        };
+        url = {
+          "git@git.sr.ht:".insteadOf = "sh:";
+          "git@codeberg.org:".insteadOf = "cb:";
+          "git@github.com:".insteadOf = "gh:";
+          "git@gitlab.com:".insteadOf = "gl:";
+          "git@git.sr.ht:~h4shcore/".insteadOf = "me@sh:";
+          "git@github.com:h4shcore/".insteadOf = "me@gh:";
+        };
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.rebase = false;
+        };
       };
+    };
 
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = false;
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "h4shcore";
+          email = "97403914+h4shcore@users.noreply.github.com";
+        };
+        ui = {
+          default-command = "log";
+        };
+        git = {
+          auto-local-bookmark = true;
+        };
       };
     };
   };
