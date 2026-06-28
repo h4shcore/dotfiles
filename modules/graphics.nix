3d3bb3f7d32e0d5f8297078767c50160f47216d7
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-28 15:57:34 +0530

    added graphics.nix

---
+++ modules/graphics.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    glxinfo
+    vulkan-tools
+    nvidia-settings
+  ];
+}
