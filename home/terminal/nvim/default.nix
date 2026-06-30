4a132be8c1aee20ff45944ee6cda9c75339e1cb2
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-30 23:26:34 +0530

    added new theme feature (wip)

---
+++ home/terminal/nvim/default.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+let
+  theme = import ../themes;
+in {
+  xdg.configFile."nvim/lua/palette.lua".text =
+    import ../themes/generators/nvim {
+      inherit theme;
+    };
+}
--- home/themes/default.nix
+++ home/themes/default.nix
@@ -1,6 +1,1 @@
 import ./ryu
-
-xdg.configFile."nvim/lua/palette.lua".text =
-  import ../themes/generators/nvim.nix {
-    inherit theme;
-  };
