ae53b8af17b1cea3b1a84d3295649c52fe42e4f0
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-09-17 23:32:42 +0530

    added doom emacs

--- home/editors/emacs/doom.d/config.el
+++ home/editors/emacs/doom.d/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
 ;; Specify both a dark and light theme, like so and Doom will choose which one
 ;; to load based on your system light/dark setting:
 ;;
@@ -40,10 +40,14 @@
 ;;
 ;; If you want more pro-active theme switching based on OS light/dark mode, look
 ;; up the `auto-dark' package.
+(add-to-list 'custom-theme-load-path "~/.config/doom/themes/")
+
+(after! doom-themes
+  (load-theme 'matugen t))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
